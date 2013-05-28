8b8772b1ad9eddca6a8cc3b6ecf04d30c3c97112
Anni S Perheentupa <asperhee@dhp-59.cs.helsinki.fi>
2013-05-28 13:52:07 +0300

    php php

---
+++ HTML/kaannoshaku.php
@@ -0,0 +1,13 @@
+<?php
+ $alkukieli = $_POST["kielesta"];
+ $kohdekieli = $_POST["kieleen"];
+ $sana = $_GET["sana"];
+ 
+ if ($sana = ""){
+	echo "<i> Ei tuloksia </i>";
+ } else {
+	// tietokantahaku
+	echo $sana;
+	echo "<br> Määritelmä: Jonkinlainen äänneyhdistelmä, jota käytetään kommunikointiin."
+ }
+?>
